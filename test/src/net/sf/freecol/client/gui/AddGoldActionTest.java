812fefd32ed3e11f7c2fd57c5cf659412ddd39b9
Eduardo <ej.dias@campus.fct.unl.pt>
2023-11-29 16:10:24 +0000

    Unit Test de Adicionar Ouro, tive que simular a adição de ouro por um evento

--- test/src/net/sf/freecol/client/gui/AddGoldActionTest.java
+++ test/src/net/sf/freecol/client/gui/AddGoldActionTest.java
@@ -1,36 +1,28 @@
 package net.sf.freecol.client.gui;
-import junit.framework.TestSuite;
-import net.sf.freecol.client.FreeColClient;
-import net.sf.freecol.client.gui.action.AddGoldAction;
-import net.sf.freecol.common.FreeColException;
+
 import net.sf.freecol.common.model.Game;
-import net.sf.freecol.common.model.Player;
+import net.sf.freecol.server.ServerTestHelper;
+import net.sf.freecol.server.model.ServerPlayer;
 import net.sf.freecol.util.test.FreeColTestCase;
-import org.junit.Test;
 
-import java.awt.*;
-import java.io.File;
 
-import static org.junit.Assert.*;
 public class AddGoldActionTest extends FreeColTestCase {
 
-    @Test
-    public void AddGoldActionTest() {
+    public void testAddGold(){
 
+        Game game = ServerTestHelper.startServerGame(getTestMap());
 
-        //Player p = game.getClientPlayer();
+        ServerPlayer player = getServerPlayer(game, "model.nation.dutch");
 
-        assertTrue(getServerPlayer(getStandardGame(), "19").checkGold(0));
-        /*
-        System.out.println(getTestMap(true));
-        System.out.println(getTestMap(false));
-        System.out.println(game.getFreeColGameObject("19"));
-        System.out.println(game.getClientPlayer().checkGold(0));
-        System.out.println(getServerPlayer(game, "19").revealMap());
+        assertTrue(player.checkGold(0));
 
-        System.out.println(p);
-*/
-        //AddGoldAction addGoldAction = new AddGoldAction(p);
+        simulateControlKeyPress(player);  //simulaçao de um evento de clique com Crtl-B por exemplo
+
+        assertTrue(player.checkGold(1000));
+    }
+
+    private void simulateControlKeyPress(ServerPlayer player){
+        player.modifyGold(1000);
     }
 
 
