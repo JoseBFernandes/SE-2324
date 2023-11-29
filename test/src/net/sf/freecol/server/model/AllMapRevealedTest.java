ecc331ced38f65d3aa1fd898969bd5a3c3402a34
Eduardo <ej.dias@campus.fct.unl.pt>
2023-11-29 14:47:39 +0000

    Teste de Revelar o mapa

---
+++ test/src/net/sf/freecol/server/model/AllMapRevealedTest.java
@@ -0,0 +1,33 @@
+package net.sf.freecol.server.model;
+
+import net.sf.freecol.common.model.Game;
+import net.sf.freecol.common.model.Map;
+import net.sf.freecol.common.model.Tile;
+import net.sf.freecol.server.ServerTestHelper;
+import net.sf.freecol.util.test.FreeColTestCase;
+
+public class AllMapRevealedTest extends FreeColTestCase {
+
+
+    public void testMapRevealComplete() {
+
+        getTestMap();
+        Game game = ServerTestHelper.startServerGame(getTestMap());
+
+
+        Map map = game.getMap();
+
+        ServerPlayer player = getServerPlayer(game, "model.nation.dutch");
+
+        player.revealMap();
+
+        Tile tile1 = map.getTile(6, 8);
+        Tile tile2 = map.getTile(8, 6);
+
+        assertTrue("Setup error, tile1 should be explored by player", player.hasExplored(tile1));
+        assertTrue("Setup error, tile2 should be explored by player", player.hasExplored(tile2));
+
+
+    }
+
+}
